aeb52b8b47855ac5060cb581bb17abe245a0c9f2
infinitengine <98674653+infinitengine@users.noreply.github.com>
2022-02-09 13:17:09 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # declare your service account variable here or within a variables.tf file
 variable "GOOGLE_CREDENTIALS" {
 	type = string
-	default = ""
+	default = "my-default-stack-c43561"
 }
 variable "stackpath_stack_id" {
 	type = string
