7fbc9cfd7968db41e4fa0d9efeb5f8c971cdaf04
infinitengine <98674653+infinitengine@users.noreply.github.com>
2022-02-08 22:37:56 -0600

    Added StackPath

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,15 @@ output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
 
+# Output a StackPath compute workload's instances' name, internal IP addresses, 
+# location, and status
+output "my-compute-workload-instances" {
+  value = {
+    for instance in stackpath_compute_workload.my-compute-workload.instances:
+    instance.name => {
+      ip_address = instance.external_ip_address
+      phase      = instance.phase
+      location   = instance.location
+    }
+  }
+}
