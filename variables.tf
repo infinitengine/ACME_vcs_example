773253ba19ff188002c2b41559971433928ec9c4
infinitengine <98674653+infinitengine@users.noreply.github.com>
2022-02-09 12:59:14 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,16 +12,3 @@ variable "instance_name" {
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
-
-variable "stackpath_stack_id" {
-	type = string
-	default = "my-default-stack-c43561"
-}
-variable "stackpath_client_id" {
-        type = string
-        default = "7d9168f4e89890e6615ce4c4b5d154dd"
-}
-variable "stackpath_client_secret" {
-        type = string
-        default = "212dd55fb3c8b91a86a3e841e595dc9c1842eebcb0f1076d1429a0ef8c0f3bc1"
-}
