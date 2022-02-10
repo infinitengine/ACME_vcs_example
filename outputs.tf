de89bb79a39864e6f50a890a4b640ca440e9c689
infinitengine <98674653+infinitengine@users.noreply.github.com>
2022-02-09 20:54:55 -0600

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,20 @@
+output "instance_ami" {
+  value = aws_instance.ubuntu.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.ubuntu.arn
+}
+
+# Output a StackPath compute workload's instances' name, internal IP addresses, 
+# location, and status
+output "my-compute-workload-instances" {
+  value = {
+    for instance in stackpath_compute_workload.erik.instances:
+    instance.name => {
+      ip_address = instance.external_ip_address
+      phase      = instance.phase
+      location   = instance.location
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-west-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform"
+}
+
+variable "GOOGLE_CREDENTIALS" {
+	type = string
+	default = ""
+}
+variable "STACKPATH_STACK_ID" {
+	type = string
+        default = "my-default-stack-c43561"
+}
+variable "STACKPATH_CLIENT_ID" {
+        type = string
+        default = "7d9168f4e89890e6615ce4c4b5d154dd"
+}
+variable "STACKPATH_CLIENT_SECRET" {
+        type = string
+        default = "212dd55fb3c8b91a86a3e841e595dc9c1842eebcb0f1076d1429a0ef8c0f3bc1"
+}
