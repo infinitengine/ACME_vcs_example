b24bf99e0027d3fcdffa2f04d0bb2f4926a235cb
infinitengine <98674653+infinitengine@users.noreply.github.com>
2022-02-09 21:18:58 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,13 +19,13 @@ variable "GOOGLE_CREDENTIALS" {
 }
 variable "stackpath_stack_id" {
 	type = string
-        default = "my-default-stack-c43561"
+        default = ""
 }
 variable "stackpath_client_id" {
         type = string
-        default = "7d9168f4e89890e6615ce4c4b5d154dd"
+        default = ""
 }
 variable "stackpath_client_secret" {
         type = string
-        default = "212dd55fb3c8b91a86a3e841e595dc9c1842eebcb0f1076d1429a0ef8c0f3bc1"
+        default = ""
 }
